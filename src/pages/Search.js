02911f48d3d5f9c0aa3d5939bd1aeb333a0b5ac4
afonsobarros <a@ignited.io>
2017-08-06 06:45:46 +0100

    search implementations

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,50 +4,73 @@ import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
 import Book from '../components/Book';
+import * as BooksAPI from '../utils/BooksAPI';
 
 class Search extends Component {
 
   state = {
-    query: ''
+    query: '',
+    searchResults: []
+  }
+  
+
+  constructor() {
+    super()
+    this.loadTimeout = 0;
   }
 
   updateQuery = (query) => {
+    clearTimeout(this.loadTimeout);
     this.setState({ query: query.trim() })
+    this.loadTimeout = setTimeout( () => this.getBooks(query), 500)
   }
 
   clearQuery = () => {
     this.setState({ query: '' })
   }
 
+  getShelfTitle = (shelf) => {
+    shelf = shelf.replace(/([A-Z])/g, ' $1').trim()
+    return shelf.charAt(0).toUpperCase() + shelf.slice(1).toLowerCase();
+  }
+
+  getBooks(query) {
+    BooksAPI.search(query).then(
+      res => {
+        console.log('search books', res)
+        if (res) {
+          this.setState({
+            searchResults: !res.error ? res : []
+          })
+        }
+      }
+    )
+  }
+
+
   render() {
 
-    const { books } = this.props;
-    const { query } = this.state;
+    const { books, shelves } = this.props;
+    const { query, searchResults } = this.state;
 
-    let filteredBooks;
+    let myReadingBooks = [];
+
     if (query) {
       const match = new RegExp(escapeRegExp(query), 'i')
-      filteredBooks = books.filter((book) => match.test(book.title))
+      myReadingBooks = books.filter((book) => match.test(book.title) || match.test(book.authors.toString()))
     } else {
-      filteredBooks = books
+      myReadingBooks = books
     }
 
-    filteredBooks.sort(sortBy('title'))
+    myReadingBooks.sort(sortBy('title'))
+    if (searchResults && searchResults.length > 0)
+      searchResults.sort(sortBy('title'))
 
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */
-            }
             <input type="text" placeholder="Search by title or author"
               value={query}
               onChange={(e) => this.updateQuery(e.target.value)}
@@ -55,43 +78,77 @@ class Search extends Component {
           </div>
         </div>
         <div className="search-books-results">
-
-          {
-            filteredBooks.length < 1 && (
-              <div>
-                <p>
-                  <span>NOTES: The search from BooksAPI is limited to a particular set of search terms. Please use one of the following: </span>
-                  <a href="https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md" target="_blank">check search terms</a>
-                </p>
-                <p>However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if you don't find a specific author or title.</p>
-                <p> Every search is limited by search terms.</p>
-              </div>
-            )
-          }
-          <ol className="books-grid">
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Online search results</h2>
             {
-              filteredBooks.map((book) => (
-                <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors.toString()}</div>
-                  </div>
-                </li>
-              ))
+              searchResults.length < 1 && this.state.query != '' && (
+                <p>
+                  No results retrieved for <b>"{this.state.query}"</b>. Try another term.
+                </p>
+              )
             }
-          </ol>
+            {
+              searchResults.length < 1 && (
+                <div>
+                  <p>
+                    <span>The search from BooksAPI is limited to a particular set of search terms. Please use one of the following: </span>
+                    <a href="https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md" target="_blank">check search terms</a>
+                  </p>
+                  <p>However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if you don't find a specific author or title.</p>
+                  <p> Every search is limited by search terms.</p>
+                </div>
+              )
+            }
+
+            {
+              searchResults && searchResults.length > 1 && (
+
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {
+                      searchResults.map((book) => (
+                        <li key={book.id}>
+                          <Book book={book} shelves={shelves} updateBook={this.props.updateBook} />
+                        </li>
+                      ))
+                    }
+                  </ol>
+                </div>
+              )
+            }
+          </div>
+
+          <div className="search-books-results">
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">My Collections</h2>
+              {
+                myReadingBooks.length < 1 && this.state.query != '' && (
+                  <div>
+                    <p>
+                      No books found in My Collections for "<b>{this.state.query}</b>". Try another term.
+                    </p>
+                  </div>
+                )
+              }
+
+              {
+                myReadingBooks.length > 1 && (
+
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {
+                        myReadingBooks.map((book) => (
+                          <li key={book.id}>
+                            <Book book={book} shelves={shelves} updateBook={this.props.updateBook} />
+                          </li>
+                        ))
+                      }
+                    </ol>
+                  </div>
+                )
+              }
+            </div>
+          </div>
         </div>
       </div>
     )

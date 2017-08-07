132ec06d851abf3a51fcd078b644c45138e12718
afonsobarros <a@ignited.io>
2017-08-07 14:26:52 +0100

    styling moved to css

--- src/components/Book.js
+++ src/components/Book.js
@@ -14,7 +14,7 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
           <div className="book-shelf-changer">
             <select value={book.shelf} onChange={e => this.props.updateBook(book, e.target.value)}>
               <option value="none" disabled>Move to...</option>

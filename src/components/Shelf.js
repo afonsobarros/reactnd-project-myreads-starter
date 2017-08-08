eece15f7a0b8303d5c9d406feb800fee8a9b67eb
afonsobarros <a@ignited.io>
2017-08-08 18:56:36 +0100

    convert shelf to a stateless component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book';
 
-class Shelf extends Component {
+const Shelf = ( props ) => {
 
-  getShelfTitle = (shelf) => {
-    shelf = shelf.replace(/([A-Z])/g, ' $1').trim()
-    return shelf.charAt(0).toUpperCase() + shelf.slice(1).toLowerCase();
-  }
+  const { books, shelves, updateBook, title } = props;
 
-  render() {
-
-    const { shelf, books, shelves, updateBook} = this.props;
-    const title = this.props.title || this.getShelfTitle(shelf);
-    
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{ title }</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {
-              books.map((book , index) => (
-                <li key={book.id + index}>
-                  <Book book={ book } shelves={ shelves } updateBook={ updateBook } />
-                </li>
-              ))
-            }
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {
+            books.map((book, index) => (
+              <li key={book.id + index}>
+                <Book book={book} shelves={shelves} updateBook={updateBook} />
+              </li>
+            ))
+          }
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Shelf;
